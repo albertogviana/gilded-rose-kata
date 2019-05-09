4f78820d51bff5682071db369b981eeccc6cb24c
Alberto Viana <viana@vesseltracker.com>
2019-05-09 14:08:33 +0200

    Adding more tests to increase the code coverage

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -19,28 +19,40 @@ var items = []Item{
 func main() {
 	fmt.Println("OMGHAI!")
 	// fmt.Print(items)
-	GildedRose()
+	GildedRose(items)
 }
 
-func GildedRose() {
+func GildedRose(items []Item) {
 	for i := 0; i < len(items); i++ {
-
+		// if item is not Aged Brie and Backstage passes to a TAFKAL80ETC concert, quality
+		// is greater than 0, and item is not Sulfuras, Hand of Ragnaros, then quality
+		// decreases 1
 		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+			// The quality of an item is never negative
 			if items[i].quality > 0 {
 				if items[i].name != "Sulfuras, Hand of Ragnaros" {
+					// At the end of each day our system lowers both values for every item
 					items[i].quality = items[i].quality - 1
 				}
 			}
 		} else {
+			// The quality of an item is never more than 50
 			if items[i].quality < 50 {
+				// "Aged Brie" actually increases in quality the older it gets
 				items[i].quality = items[i].quality + 1
+				// "Backstage passes", like aged brie, increases in quality as its sell-in
+				// value approaches; quality increases by 2 when there are 10 days or less
+				// and by 3 when there are 5 days or less but quality drops to 0 after the
+				// concert
 				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
 					if items[i].sellIn < 11 {
+						// The quality of an item is never more than 50
 						if items[i].quality < 50 {
 							items[i].quality = items[i].quality + 1
 						}
 					}
 					if items[i].sellIn < 6 {
+						// The quality of an item is never more than 50
 						if items[i].quality < 50 {
 							items[i].quality = items[i].quality + 1
 						}
@@ -50,22 +62,28 @@ func GildedRose() {
 		}
 
 		if items[i].name != "Sulfuras, Hand of Ragnaros" {
+			// At the end of each day our system lowers both [sell in and quality] values for every item
 			items[i].sellIn = items[i].sellIn - 1
 		}
 
 		if items[i].sellIn < 0 {
 			if items[i].name != "Aged Brie" {
 				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+					// The quality of an item is never negative
 					if items[i].quality > 0 {
 						if items[i].name != "Sulfuras, Hand of Ragnaros" {
+							// At the end of each day our system lowers both values for every item
 							items[i].quality = items[i].quality - 1
 						}
 					}
 				} else {
+					// "Backstage passes" quality drops to 0 after the concert
 					items[i].quality = items[i].quality - items[i].quality
 				}
 			} else {
+				// The quality of an item is never more than 50
 				if items[i].quality < 50 {
+					// "Aged Brie" actually increases in quality the older it gets
 					items[i].quality = items[i].quality + 1
 				}
 			}
--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -1,7 +1,111 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_GildedRose(t *testing.T) {
-	main()
+// At the end of each day our system lowers both values for every item
+func TestNormalItemDecreaseSellInByOne(t *testing.T) {
+	testUpdateSellIn(t, "normal item", 10, 20, 9)
+}
+
+// At the end of each day our system lowers both values for every item
+func TestNormalItemDecreaseQualityByOne(t *testing.T) {
+	testUpdateQuality(t, "normal item", 10, 20, 19)
+}
+
+// Once the sell by date has passed, quality degrades twice as fast
+func TestNormalItemQualityDegradesTwiceAsFastOnceTheSellDatePassed(t *testing.T) {
+	testUpdateQuality(t, "normal item", 0, 20, 18)
+}
+
+// The quality of an item is never negative
+func TestNormalItemQualityIsNeverNegative(t *testing.T) {
+	testUpdateQuality(t, "normal item", 10, 0, 0)
+}
+
+// "Aged Brie" actually increases in quality the older it gets
+func TestAgedBrieIncreasesQualityTheOlderItGetsByOne(t *testing.T) {
+	testUpdateQuality(t, "Aged Brie", 2, 0, 1)
+}
+
+// "Aged Brie" actually increases in quality the older it gets
+func TestAgedBrieIncreasesQualityByTwoAfterSellDatePassed(t *testing.T) {
+	testUpdateQuality(t, "Aged Brie", 0, 30, 32)
+}
+
+// The quality of an item is never more than 50
+func TestAgedBrieQualityIsNeverMoreThanFifty(t *testing.T) {
+	testUpdateQuality(t, "Aged Brie", 2, 50, 50)
+}
+
+// "Sulfuras", being a legendary item, never has to be sold or decreases in quality
+func TestSufurasNeverDecreasesQuality(t *testing.T) {
+	testUpdateQuality(t, "Sulfuras, Hand of Ragnaros", 0, 80, 80)
+}
+
+// "Sulfuras", being a legendary item, never has to be sold or decreases in quality
+func TestSufurasNeverDecreasesSellIn(t *testing.T) {
+	testUpdateSellIn(t, "Sulfuras, Hand of Ragnaros", 0, 80, 0)
+}
+
+func TestBackstageQualityIncreasesByOne(t *testing.T) {
+	testUpdateQuality(t, "Backstage passes to a TAFKAL80ETC concert", 15, 20, 21)
+}
+
+// "Backstage passes", like aged brie, increases in quality as its sell-in
+// value approaches; quality increases by 2 when there are 10 days or less
+func TestBackstageQualityIncreasesByTwoWhen10DaysOrLess(t *testing.T) {
+	testUpdateQuality(t, "Backstage passes to a TAFKAL80ETC concert", 10, 20, 22)
+}
+
+// "Backstage passes", like aged brie, increases in quality as its sell-in
+// value approaches; quality increases and by 3 when there are 5 days or less
+func TestBackstageQualityIncreasesByThreeWhen5DaysOrLess(t *testing.T) {
+	testUpdateQuality(t, "Backstage passes to a TAFKAL80ETC concert", 5, 20, 23)
+}
+
+// "Backstage passes" quality drops to 0 after the concert
+func TestBackstageQualityDropsAfterConcert(t *testing.T) {
+	testUpdateQuality(t, "Backstage passes to a TAFKAL80ETC concert", 0, 20, 0)
+}
+
+// At the end of each day our system lowers both values for every item
+func TestConjuredDecreasesQualityByOne(t *testing.T) {
+	testUpdateQuality(t, "Conjured Mana Cake", 3, 6, 5)
+}
+
+// At the end of each day our system lowers both values for every item
+func TestConjuredDecreasesSellInByOne(t *testing.T) {
+	testUpdateSellIn(t, "Conjured Mana Cake", 3, 6, 2)
+}
+
+// "Conjured" items degrade in quality twice as fast as normal items
+func TestConjuredDecreasesQualityByTwo(t *testing.T) {
+	testUpdateQuality(t, "Conjured Mana Cake", 0, 6, 4)
+}
+
+// helpers
+func testUpdateQuality(t *testing.T, itemName string, itemSellIn int, itemQuality int, expectedQuality int) {
+	var items = []Item{
+		Item{itemName, itemSellIn, itemQuality},
+	}
+
+	GildedRose(items)
+
+	if items[0].quality != expectedQuality {
+		t.Errorf("Quality (expected: %d, actual: %d).", expectedQuality, items[0].quality)
+	}
+}
+
+func testUpdateSellIn(t *testing.T, itemName string, itemSellIn int, itemQuality int, expectedSellIn int) {
+	var items = []Item{
+		Item{itemName, itemSellIn, itemQuality},
+	}
+
+	GildedRose(items)
+
+	if items[0].sellIn != expectedSellIn {
+		t.Errorf("SellIn (expected: %d, actual: %d).", expectedSellIn, items[0].sellIn)
+	}
 }
