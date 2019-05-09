dbcdc036044746806735e1010b5c103ffc208a58
Alberto Viana <viana@vesseltracker.com>
2019-05-10 00:20:45 +0200

    Refactory the GildedRose and decreasing the cyclomatic complexity

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -24,70 +24,47 @@ func main() {
 
 func GildedRose(items []Item) {
 	for i := 0; i < len(items); i++ {
-		// if item is not Aged Brie and Backstage passes to a TAFKAL80ETC concert, quality
-		// is greater than 0, and item is not Sulfuras, Hand of Ragnaros, then quality
-		// decreases 1
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			// The quality of an item is never negative
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					// At the end of each day our system lowers both values for every item
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
+		if items[i].name == "Aged Brie" || items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+			// At the end of each day our system lowers both values for every item
+			items[i].sellIn = items[i].sellIn - 1
+
 			// The quality of an item is never more than 50
 			if items[i].quality < 50 {
-				// "Aged Brie" actually increases in quality the older it gets
 				items[i].quality = items[i].quality + 1
-				// "Backstage passes", like aged brie, increases in quality as its sell-in
-				// value approaches; quality increases by 2 when there are 10 days or less
-				// and by 3 when there are 5 days or less but quality drops to 0 after the
-				// concert
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						// The quality of an item is never more than 50
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						// The quality of an item is never more than 50
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
 			}
-		}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			// At the end of each day our system lowers both [sell in and quality] values for every item
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					// The quality of an item is never negative
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							// At the end of each day our system lowers both values for every item
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
+			if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+				if items[i].sellIn < 0 {
 					// "Backstage passes" quality drops to 0 after the concert
 					items[i].quality = items[i].quality - items[i].quality
 				}
-			} else {
-				// The quality of an item is never more than 50
-				if items[i].quality < 50 {
-					// "Aged Brie" actually increases in quality the older it gets
+				if items[i].sellIn < 11 && items[i].quality > 0 && items[i].quality < 50 {
+					items[i].quality = items[i].quality + 1
+				}
+				if items[i].sellIn < 6 && items[i].quality > 0 && items[i].quality < 50 {
 					items[i].quality = items[i].quality + 1
 				}
 			}
+
+			if items[i].sellIn < 0 && items[i].quality > 0 && items[i].quality < 50 {
+				items[i].quality = items[i].quality + 1
+			}
+		} else {
+			// The quality of an item is never negative
+			if items[i].quality > 0 && items[i].name != "Sulfuras, Hand of Ragnaros" {
+				// At the end of each day our system lowers both values for every item
+				items[i].quality = items[i].quality - 1
+			}
+
+			if items[i].name != "Sulfuras, Hand of Ragnaros" {
+				items[i].sellIn = items[i].sellIn - 1
+			}
+
+			if items[i].sellIn < 0 && items[i].quality > 0 && items[i].name != "Sulfuras, Hand of Ragnaros" {
+				items[i].quality = items[i].quality - 1
+			}
 		}
+
 	}
 
 }
